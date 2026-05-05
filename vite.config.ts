81edcd72815b489eb4d7be6c8b3c1438753a1410
zox <153457825+zoxile@users.noreply.github.com>
2026-05-05 03:10:44 +0200

    chore(alias): Added aliases for different folders
    - add path aliases for folders within lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import path from "path";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [svelte(), tailwindcss()],
+	resolve: {
+		alias: {
+			$lib: path.resolve("./src/lib"),
+		},
+	},
 });
