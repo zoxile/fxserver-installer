85118ba758a3cd53c9e08e809a35f11e98493262
zox <153457825+zoxile@users.noreply.github.com>
2026-05-05 03:14:41 +0200

    chore(ui): add tailwind & shadcn-svelte
    - install and configure tailwindcss v4
    - install and initialize shadcn-svelte
    - add base shadcn UI components

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import Titlebar from "./Titlebar.svelte";
 </script>
 
+<Titlebar></Titlebar>
 <span class="font-mono">Initial</span>
 <span class="font-serif">GitHub</span>
 <span class="font-sans">Commit</span>
